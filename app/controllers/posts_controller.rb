195ee6764723712eb7b8d32408c99959a12a8f76
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-30 13:46:41 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+  end 
+
+  def create
+    redirect_to posts_path 
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  # resources :posts, only: [:index, :new]
+
+  
+  get '/posts', to: 'posts#index', as: 'posts'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
+
+
 end
